9eaba4c980b0066da6b74c4f59f6879617687071
Shalom <shalom2552@gmail.com>
2026-03-31 22:32:56 +0300

    doc(input): add documantation to input

--- inc/utils/input.hpp
+++ inc/utils/input.hpp
@@ -3,12 +3,16 @@
 
 #include "../types.hpp"
 
+/* enable teminal raw mode */
 void input_setup();
 
+/* restore original terminal settings */
 void cleanup();
 
+/* trap <C-c> to call cleanup */
 void handle_sigint(int sig);
 
+/* parse user input to input type */
 InputEvent get_input();
 
 #endif // !UTILS_INPUT_HPP
