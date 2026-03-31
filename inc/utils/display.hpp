35b2ef950a867349783a615fe36c3c406cc5611a
Shalom <shalom2552@gmail.com>
2026-03-31 18:01:32 +0300

    refactor(display): refactor box rows display functions

--- inc/utils/display.hpp
+++ inc/utils/display.hpp
@@ -2,25 +2,30 @@
 #define UTILS_DISPLAY_HPP
 
 #include "../board.hpp"
+#include "../types.hpp"     // Ansi
 
 #include <cstddef>  // std::size_t
 #include <string>   // std::string
 
 void clear_screen();
 
+void print_char(std::string const& c);
+
 void print_board(Board const& Board);
 
 std::size_t get_printable_size(std::size_t size);
 
-void print_char(std::string const& c);
+void print_board_row(std::size_t size, std::vector<Cell> const& row);
+
+void print_box_row(std::size_t size, Ansi left, Ansi sep, Ansi mid, Ansi right);
 
 void print_top_border(std::size_t size);
 
-void print_row(std::size_t size, std::vector<Cell> const& row);
+void print_bottom_border(std::size_t size);
 
 void print_border_row(std::size_t size);
 
-void print_bottom_border(std::size_t size);
+void print_buffer_row(std::size_t size);
 
 #endif // !UTILS_DISPLAY_HPP
 
--- src/utils/display.cpp
+++ src/utils/display.cpp
@@ -1,15 +1,21 @@
 #include "../../inc/utils/display.hpp"
 #include "../../inc/constants.hpp"
 #include "../../inc/board.hpp"
+#include "../../inc/types.hpp"
 
 #include <cstddef>  // std::size_t
 #include <iostream> // std::cout
-#include <string>
+#include <string>   // std::string
 #include <vector>   // std::vector
 
 void clear_screen()
 {
     print_char(ANSI_CLEAR);
+}
+
+void print_char(std::string const& c)
+{
+    std::cout << c;
 }
 
 void print_board(Board const& board)
@@ -18,7 +24,7 @@ void print_board(Board const& board)
     print_top_border(size);
     for (std::size_t i = 0; i < size; ++i) {
         if (i > 0) print_border_row(size);
-        print_row(size, board.get_board()[i]);
+        print_board_row(size, board.get_board()[i]);
     }
     print_bottom_border(size);
 }
@@ -29,24 +35,7 @@ std::size_t get_printable_size(std::size_t size)
     return 4 * size + 1;
 }
 
-void print_char(std::string const& c)
-{
-    std::cout << c;
-}
-
-void print_top_border(std::size_t size)
-{
-    size = get_printable_size(size);
-    print_char(ANSII_BOX_TL);
-    for (std::size_t i = 1; i < size - 1; ++i) {
-        if (i % 4 == 0) print_char(ANSII_BOX_TM);
-        else print_char(ANSII_BOX_HZ);
-    }
-    print_char(ANSII_BOX_TR);
-    print_char("\n");
-}
-
-void print_row(std::size_t size, std::vector<Cell> const& row)
+void print_board_row(std::size_t size, std::vector<Cell> const& row)
 {
     print_char(ANSII_BOX_VR);
     size = get_printable_size(size);
@@ -73,27 +62,30 @@ void print_row(std::size_t size, std::vector<Cell> const& row)
     print_char("\n");
 }
 
-void print_border_row(std::size_t size)
+void print_top_border(std::size_t size)
 {
-    size = get_printable_size(size);
-    print_char(ANSII_BOX_VR);
-    for (std::size_t i = 1; i < size - 1; ++i) {
-        if (i % 4 == 0) print_char(ANSII_BOX_CR);
-        else print_char(ANSII_BOX_HZ);
-    }
-    print_char(ANSII_BOX_VR);
-    print_char("\n");
+    print_box_row(size, ANSII_BOX_TL, ANSII_BOX_TM, ANSII_BOX_HZ, ANSII_BOX_TR);
 }
 
 void print_bottom_border(std::size_t size)
 {
+    print_box_row(size, ANSII_BOX_BL, ANSII_BOX_BM, ANSII_BOX_HZ, ANSII_BOX_BR);
+}
+
+void print_border_row(std::size_t size)
+{
+    print_box_row(size, ANSII_BOX_VR, ANSII_BOX_CR, ANSII_BOX_HZ, ANSII_BOX_VR);
+}
+
+void print_box_row(std::size_t size, Ansi left, Ansi sep, Ansi mid, Ansi right)
+{
     size = get_printable_size(size);
-    print_char(ANSII_BOX_BL);
+    print_char(left);
     for (std::size_t i = 1; i < size - 1; ++i) {
-        if (i % 4 == 0) print_char(ANSII_BOX_BM);
-        else print_char(ANSII_BOX_HZ);
+        if (i % 4 == 0) print_char(sep);
+        else print_char(mid);
     }
-    print_char(ANSII_BOX_BR);
+    print_char(right);
     print_char("\n");
 }
 
